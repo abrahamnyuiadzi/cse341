0b524a5117096d49693149ddb5d2fe07396c7b30
*abrahamnyuiadzi* <abrahamnyuiadzi>
2024-09-20 19:39:53 +0000

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("abraham");
+  res.send("abruujuaham");
 });
  
 app.listen(process.env.PORT || 3000, () => {
